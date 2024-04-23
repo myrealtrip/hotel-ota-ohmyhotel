91531532f37b98f599f4e1b4c3e400582a7d34f4
강민수 <minsu.kang@myrealtrip.com>
2024-04-23 13:29:29 +0900

    로깅추가

--- api/src/main/java/com/myrealtrip/ohmyhotel/api/presentation/search/SearchController.java
+++ api/src/main/java/com/myrealtrip/ohmyhotel/api/presentation/search/SearchController.java
@@ -3,6 +3,7 @@ package com.myrealtrip.ohmyhotel.api.presentation.search;
 import com.myrealtrip.common.values.Resource;
 import com.myrealtrip.ohmyhotel.api.application.search.OrderSearchService;
 import com.myrealtrip.ohmyhotel.api.application.search.SearchService;
+import com.myrealtrip.srtcommon.support.utils.ObjectMapperUtils;
 import com.myrealtrip.unionstay.dto.hotelota.search.request.SearchRequest;
 import com.myrealtrip.unionstay.dto.hotelota.search.response.SearchResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class SearchController {
 
     private final SearchService searchService;
@@ -33,7 +34,9 @@ public class SearchController {
     @Operation(summary = "실시간 재고 및 가격 조회 (주문서 조회 용도)", description = "property 의 실시간 재고와 가격을 검색합니다. (주문서 조회 용도)")
     @GetMapping(value = "/properties/search/reservation")
     public Resource<SearchResponse> searchForOrder(@Valid SearchRequest searchRequest) {
+        log.info("{}", ObjectMapperUtils.writeAsString(searchRequest));
         SearchResponse searchResponse = orderSearchService.search(searchRequest);
+        log.info("{}", ObjectMapperUtils.writeAsString(searchResponse));
         return Resource.<SearchResponse>builder()
             .data(searchResponse)
             .build();
