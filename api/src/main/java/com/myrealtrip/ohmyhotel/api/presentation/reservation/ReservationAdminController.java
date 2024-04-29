f7ce4fbead4b5866782253b2e86e2e09354bddec
강민수 <minsu.kang@myrealtrip.com>
2024-04-29 18:35:08 +0900

    [SRTT-3034] 누락된 코드 추가

--- api/src/main/java/com/myrealtrip/ohmyhotel/api/presentation/reservation/ReservationAdminController.java
+++ api/src/main/java/com/myrealtrip/ohmyhotel/api/presentation/reservation/ReservationAdminController.java
@@ -1,6 +1,7 @@
 package com.myrealtrip.ohmyhotel.api.presentation.reservation;
 
 import com.myrealtrip.common.values.Resource;
+import com.myrealtrip.ohmyhotel.api.application.reservation.ReservationAdminService;
 import com.myrealtrip.ohmyhotel.core.service.reservation.BookingMessageKafkaSendService;
 import com.myrealtrip.ohmyhotel.enumarate.ReservationStatus;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,12 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationAdminController {
 
     private final BookingMessageKafkaSendService bookingMessageKafkaSendService;
+    private final ReservationAdminService reservationAdminService;
 
     @Operation(summary = "예약 상태 강제변경 API", description = "에약의 상태값을 강제로 변경한다.")
     @PostMapping(value = "/admin/reservation/{mrtReservationNo}/force-status-update/{status}")
     public Resource<Void> sendReservation(@PathVariable("mrtReservationNo") String mrtReservationNo,
                                           @PathVariable("status") ReservationStatus status) {
-        bookingMessageKafkaSendService.sendByMrtReservationNo(mrtReservationNo, null, 0);
+        reservationAdminService.forceStatusUpdate(mrtReservationNo, status);
         return Resource.<Void>builder()
             .data(null)
             .build();
