7e6ebc3ed0ee1ac51fa0eabeed8c21474d53dcc9
강민수 <minsu.kang@myrealtrip.com>
2024-05-09 12:46:54 +0900

    예약취소시 이미취소되었는지 여부 확인

--- consumer/src/test/java/com/myrealtrip/ohmyhotel/consumer/reservation/service/CancelConsumeServiceTest.java
+++ consumer/src/test/java/com/myrealtrip/ohmyhotel/consumer/reservation/service/CancelConsumeServiceTest.java
@@ -7,10 +7,14 @@ import com.myrealtrip.ohmyhotel.core.domain.reservation.dto.Reservation;
 import com.myrealtrip.ohmyhotel.core.provider.reservation.ReservationProvider;
 import com.myrealtrip.ohmyhotel.core.service.reservation.ReservationApiLogService;
 import com.myrealtrip.ohmyhotel.enumarate.CanceledBy;
+import com.myrealtrip.ohmyhotel.enumarate.OmhBookingStatus;
 import com.myrealtrip.ohmyhotel.enumarate.RateType;
 import com.myrealtrip.ohmyhotel.enumarate.ReservationStatus;
 import com.myrealtrip.ohmyhotel.outbound.agent.ota.exception.OmhApiException;
+import com.myrealtrip.ohmyhotel.outbound.agent.ota.reservation.OmhBookingDetailAgent;
 import com.myrealtrip.ohmyhotel.outbound.agent.ota.reservation.OmhCancelBookingAgent;
+import com.myrealtrip.ohmyhotel.outbound.agent.ota.reservation.protocol.response.OmhBookingDetailResponse;
+import com.myrealtrip.ohmyhotel.outbound.agent.ota.reservation.protocol.response.OmhBookingDetailResponse.OmhBookingDetailAmount;
 import com.myrealtrip.ohmyhotel.outbound.agent.ota.reservation.protocol.response.OmhCancelBookingResponse;
 import com.myrealtrip.ohmyhotel.outbound.slack.sender.reservation.ReservationSlackEvent;
 import com.myrealtrip.ohmyhotel.outbound.slack.sender.reservation.ReservationSlackSender;
@@ -51,11 +55,12 @@ class CancelConsumeServiceTest {
     @Mock private ReservationApiLogService reservationApiLogService;
     @Spy private BookingOrderMessageConverter bookingOrderMessageConverter;
     @Mock private ReservationSlackSender reservationSlackSender;
+    @Mock private OmhBookingDetailAgent omhBookingDetailAgent;
 
     @BeforeEach
      void beforeEach() {
-        devCancelConsumeService = new CancelConsumeService(omhCancelBookingAgent, reservationProvider, reservationApiLogService, bookingOrderMessageConverter, reservationSlackSender, "dev");
-        prodCancelConsumeService = new CancelConsumeService(omhCancelBookingAgent, reservationProvider, reservationApiLogService, bookingOrderMessageConverter, reservationSlackSender, "prod");
+        devCancelConsumeService = new CancelConsumeService(omhCancelBookingAgent, omhBookingDetailAgent, reservationProvider, reservationApiLogService, bookingOrderMessageConverter, reservationSlackSender, "dev");
+        prodCancelConsumeService = new CancelConsumeService(omhCancelBookingAgent, omhBookingDetailAgent, reservationProvider, reservationApiLogService, bookingOrderMessageConverter, reservationSlackSender, "prod");
     }
 
     @Test
@@ -76,6 +81,28 @@ class CancelConsumeServiceTest {
     }
 
     @Test
+    @DisplayName("오마이호텔에서 이미 취소된 예약이라면 예약취소성공 상태로 변경한다.")
+    void case_1_2() {
+        // given
+        Reservation reservation = createReservation(ReservationStatus.RESERVE_CONFIRM);
+        given(reservationProvider.getByMrtReservationNoWithLock(MRT_RESERVATION_NO))
+            .willReturn(reservation);
+
+        BookingOrderMessage message = createMessage(BigDecimal.valueOf(0));
+
+        OmhBookingDetailResponse omhBookingDetailResponse = createOmhBookingDetailResponse(OmhBookingStatus.CANCELLED, BigDecimal.valueOf(0));
+        given(omhBookingDetailAgent.bookingDetail(MRT_RESERVATION_NO))
+            .willReturn(omhBookingDetailResponse);
+
+        // when
+        prodCancelConsumeService.consume(message);
+
+        // then
+        verify(reservationProvider, times(1))
+            .cancelSuccess(reservation.getReservationId(), CanceledBy.TRAVELER, message.getCancelReason(), message.getCancelReasonType(), null, BigDecimal.valueOf(0), BigDecimal.valueOf(0));
+    }
+
+    @Test
     @DisplayName("오마이호텔 예약취소 API 에 성공했다면 예약취소성공 상태로 변경한다.")
     void case_2() {
         // given
@@ -84,6 +111,10 @@ class CancelConsumeServiceTest {
             .willReturn(reservation);
 
         BookingOrderMessage message = createMessage(BigDecimal.valueOf(0));
+
+        OmhBookingDetailResponse omhBookingDetailResponse = createOmhBookingDetailResponse(OmhBookingStatus.CONFIRMED, BigDecimal.valueOf(10000));
+        given(omhBookingDetailAgent.bookingDetail(MRT_RESERVATION_NO))
+            .willReturn(omhBookingDetailResponse);
 
         OmhCancelBookingResponse omhCancelBookingResponse = createOmhCancelBookingResponse(BigDecimal.valueOf(10000));
         given(omhCancelBookingAgent.cancelBooking(MRT_RESERVATION_NO))
@@ -107,6 +138,10 @@ class CancelConsumeServiceTest {
 
         BookingOrderMessage message = createMessage(BigDecimal.valueOf(0));
 
+        OmhBookingDetailResponse omhBookingDetailResponse = createOmhBookingDetailResponse(OmhBookingStatus.CONFIRMED, BigDecimal.valueOf(10000));
+        given(omhBookingDetailAgent.bookingDetail(MRT_RESERVATION_NO))
+            .willReturn(omhBookingDetailResponse);
+
         OmhCancelBookingResponse omhCancelBookingResponse = createOmhCancelBookingResponse(null);
         given(omhCancelBookingAgent.cancelBooking(MRT_RESERVATION_NO))
             .willReturn(omhCancelBookingResponse);
@@ -128,6 +163,10 @@ class CancelConsumeServiceTest {
             .willReturn(reservation);
 
         BookingOrderMessage message = createMessage(BigDecimal.valueOf(0));
+
+        OmhBookingDetailResponse omhBookingDetailResponse = createOmhBookingDetailResponse(OmhBookingStatus.CONFIRMED, BigDecimal.valueOf(10000));
+        given(omhBookingDetailAgent.bookingDetail(MRT_RESERVATION_NO))
+            .willReturn(omhBookingDetailResponse);
 
         given(omhCancelBookingAgent.cancelBooking(MRT_RESERVATION_NO))
             .willThrow(OmhApiException.class);
@@ -153,6 +192,10 @@ class CancelConsumeServiceTest {
 
         BookingOrderMessage message = createMessage(BigDecimal.valueOf(0));
 
+        OmhBookingDetailResponse omhBookingDetailResponse = createOmhBookingDetailResponse(OmhBookingStatus.CONFIRMED, BigDecimal.valueOf(10000));
+        given(omhBookingDetailAgent.bookingDetail(MRT_RESERVATION_NO))
+            .willReturn(omhBookingDetailResponse);
+
         given(omhCancelBookingAgent.cancelBooking(MRT_RESERVATION_NO))
             .willThrow(ReadTimeoutException.class);
 
@@ -165,6 +208,13 @@ class CancelConsumeServiceTest {
 
         verify(reservationSlackSender, times(1))
             .sendToSrtWithMention(ReservationSlackEvent.CANCEL_FAIL, MRT_RESERVATION_NO, ObjectMapperUtils.writeAsString(message));
+    }
+
+    private OmhBookingDetailResponse createOmhBookingDetailResponse(OmhBookingStatus status, BigDecimal totalNetAmount) {
+        return OmhBookingDetailResponse.builder()
+            .status(status)
+            .amount(OmhBookingDetailAmount.builder().totalNetAmount(totalNetAmount).build())
+            .build();
     }
 
     private OmhCancelBookingResponse createOmhCancelBookingResponse(BigDecimal cancelPenaltyAmount) {
