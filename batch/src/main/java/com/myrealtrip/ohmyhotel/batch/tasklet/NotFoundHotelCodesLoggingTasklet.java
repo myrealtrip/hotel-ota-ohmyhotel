103a12f4f5d8c3b50ccf83183ddbbbb2cf4ffeb6
강민수 <minsu.kang@myrealtrip.com>
2024-05-22 09:46:18 +0900

    누락된 코드 추가

--- batch/src/main/java/com/myrealtrip/ohmyhotel/batch/tasklet/NotFoundHotelCodesLoggingTasklet.java
+++ batch/src/main/java/com/myrealtrip/ohmyhotel/batch/tasklet/NotFoundHotelCodesLoggingTasklet.java
@@ -3,18 +3,27 @@ package com.myrealtrip.ohmyhotel.batch.tasklet;
 
 import com.myrealtrip.ohmyhotel.batch.storage.HotelCodeStorage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
+@Slf4j
 public class NotFoundHotelCodesLoggingTasklet implements Tasklet {
 
     private final HotelCodeStorage notFoundHotelCodesStorage;
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        return null;
+        String notFoundHotelIds = notFoundHotelCodesStorage.getHotelCodes().stream()
+            .map(String::valueOf)
+            .collect(Collectors.joining("\n"));
+
+        log.info("notFoundHotelIds: \n{}", notFoundHotelIds);
+        return RepeatStatus.FINISHED;
     }
 }
