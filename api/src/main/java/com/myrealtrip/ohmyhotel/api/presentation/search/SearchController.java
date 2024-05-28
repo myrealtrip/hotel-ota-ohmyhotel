cc1b2fd597766f0e7ab4a5cc9500abb13d39de4b
강민수 <minsu.kang@myrealtrip.com>
2024-05-28 20:25:15 +0900

    로깅 추가

--- api/src/main/java/com/myrealtrip/ohmyhotel/api/presentation/search/SearchController.java
+++ api/src/main/java/com/myrealtrip/ohmyhotel/api/presentation/search/SearchController.java
@@ -9,10 +9,12 @@ import com.myrealtrip.unionstay.dto.hotelota.search.response.SearchResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +26,13 @@ public class SearchController {
     @Operation(summary = "실시간 재고 및 가격 조회", description = "property 의 실시간 재고와 가격을 검색합니다.")
     @GetMapping(value = "/properties/search")
     public Resource<SearchResponse> search(@Valid SearchRequest searchRequest) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         SearchResponse searchResponse = searchService.search(searchRequest);
+        stopWatch.stop();
+        log.info("nights: {}, execute time: {}",
+                 searchRequest.getCheckin().until(searchRequest.getCheckout(), ChronoUnit.DAYS),
+                 stopWatch.getTotalTimeSeconds());
         return Resource.<SearchResponse>builder()
             .data(searchResponse)
             .build();
