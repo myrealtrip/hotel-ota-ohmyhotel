9da13248aa3665e8d48e7df793fa5c3129e4c0e3
강민수 <minsu.kang@myrealtrip.com>
2024-07-05 13:36:56 +0900

    [SRTT-3300] 정산 API 실시간 호출로 변경

--- core/src/main/java/com/myrealtrip/ohmyhotel/core/service/CommissionRateService.java
+++ core/src/main/java/com/myrealtrip/ohmyhotel/core/service/CommissionRateService.java
@@ -1,9 +1,13 @@
 package com.myrealtrip.ohmyhotel.core.service;
 
+import com.myrealtrip.ohmyhotel.core.config.cache.annotation.LocalCacheable;
 import com.myrealtrip.ohmyhotel.core.domain.partner.dto.MrtCommissionInfo;
 import com.myrealtrip.ohmyhotel.core.domain.partner.dto.Partner;
 import com.myrealtrip.ohmyhotel.core.provider.partner.PartnerProvider;
+import com.myrealtrip.ohmyhotel.outbound.agent.mrt.settle.SettleConfigAgent;
+import com.myrealtrip.settle.web.values.SaleCommissionPolicyInquiryResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +16,7 @@ import java.math.BigDecimal;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class CommissionRateService {
 
     @Value("${ohmyhotel.partner-id}")
@@ -19,14 +24,38 @@ public class CommissionRateService {
 
     private final PartnerProvider partnerProvider;
 
-    @Transactional(readOnly = true)
-    public BigDecimal getMrtCommissionRate() {
-        return partnerProvider.getByPartnerId(partnerId).getCommissionRate();
-    }
+    private final SettleConfigAgent settleConfigAgent;
 
     @Transactional
+    public BigDecimal getMrtCommissionRate() {
+        return getMrtCommissionInfo().getCommissionRate();
+    }
+
+    /**
+     * 정산 API 로 부터 커미션 설정을 가져온다.
+     * 실패할 경우 DB 에 저장해둔 백업본을 사용한다.
+     * @return
+     */
+    @Transactional
     public MrtCommissionInfo getMrtCommissionInfo() {
-        Partner partner =  partnerProvider.getByPartnerId(partnerId);
-        return new MrtCommissionInfo(partner.getCommissionRate(), partner.getSaleCommissionPolicyId());
+        MrtCommissionInfo mrtCommissionInfo;
+        try {
+            mrtCommissionInfo = getMrtCommissionInfoFromSettlementApi();
+        } catch (Throwable t) {
+            log.error("settle api error", t);
+            Partner partner =  partnerProvider.getByPartnerId(partnerId);
+            return new MrtCommissionInfo(partner.getCommissionRate(), -1L);
+        }
+
+        if (mrtCommissionInfo.getSaleCommissionPolicyId() == -1) {
+            Partner partner =  partnerProvider.getByPartnerId(partnerId);
+            return new MrtCommissionInfo(partner.getCommissionRate(), -1L);
+        }
+        return mrtCommissionInfo;
+    }
+
+    private MrtCommissionInfo getMrtCommissionInfoFromSettlementApi() {
+        SaleCommissionPolicyInquiryResponse response = settleConfigAgent.getSettlementConfig(String.valueOf(partnerId));
+        return new MrtCommissionInfo(response.getCommissionRate(), response.getSaleCommissionPolicyId());
     }
 }
